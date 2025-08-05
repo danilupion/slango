059e53717ecc061e1c17cd3362708281994b2d63
Dani Lupión <danilupion@gmail.com>
2025-08-05 17:52:55 +0200

    Dependencies bump

--- configs/lint-staged/src/commands.js
+++ configs/lint-staged/src/commands.js
@@ -6,6 +6,6 @@ export const eslintFix =
 // This is a function because tsc --noEmit cannot be used with files (which is the default behavior of lint-staged)
 export const typescriptBuildCheck = () => 'tsc --noEmit';
 
-export const vitest = 'vitest related --run';
+export const vitest = (files) => ['vitest', 'related', '--run', ...files];
 
 export const fail = 'exit 1';
